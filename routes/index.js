df9e813f27e73a968b03332ada4e721f3a897cfb
Benoit Sauvage <benoit.sauvage44@gmail.com>
2015-12-04 10:50:08 +0100

    connection to db

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,21 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var mysql   = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'REST_DB'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM Projet', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows.length);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
