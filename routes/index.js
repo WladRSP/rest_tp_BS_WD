0fecd88442c08519897914e5dc0fca7f12497e5f
Benoit Sauvage <benoit.sauvage44@gmail.com>
2015-12-04 11:11:53 +0100

    mysql

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var router  = express.Router();
-var mysql   = require('mysql');
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'REST_DB'
-});
-
-connection.connect();
-
-connection.query('SELECT * FROM Projet', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows.length);
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
 
 module.exports = router;
---
+++ routes/projects.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router  = express.Router();
+var mysql   = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'REST_DB'
+});
+
+connection.connect();
+
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * FROM Projet', function(err, projects, fields) {
+    if (err) throw err;
+    console.log(projects);
+    res.render('projects', { projects: projects });
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  console.log(req);
+  connection.query('SELECT * FROM Projet WHERE idProjet = ' + req.id, function(err, projects, fields) {
+    if (err) throw err;
+    console.log()
+    res.render('project', { project: project });
+  });
+});
+
+module.exports = router;
